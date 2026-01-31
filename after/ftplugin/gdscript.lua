5891dfbc53de4e9972760756cc20dd6fa0c39da3
Corey Barr <coreybarr01@gmail.com>
2026-01-31 19:10:36 +1100

    Godot lsp add check for windows

--- after/ftplugin/gdscript.lua
+++ after/ftplugin/gdscript.lua
@@ -1,12 +1,19 @@
-local port = os.getenv('GDScript_Port') or '6005'
-local cmd = {'ncat', '127.0.0.1', port}
+local is_windows = vim.fn.has 'win32' == 1
+
+local port = os.getenv 'GDScript_Port' or '6005'
+
+local cmd
+if is_windows then
+  cmd = { 'ncat', '--no-shutdown', '127.0.0.1', '6005' }
+else
+  cmd = { 'nc', '127.0.0.1', '6005' }
+end
+
 local pipe = [[\\.\pipe\godot.pipe]]
 
-vim.lsp.start({
+vim.lsp.start {
   name = 'Godot',
   cmd = cmd,
   root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
-  on_attach = function(client, bufnr)
-    vim.api.nvim_command([[echo serverstart(']] .. pipe .. [[')]])
-  end
-})
+  on_attach = function(client, bufnr) vim.api.nvim_command([[echo serverstart(']] .. pipe .. [[')]]) end,
+}
