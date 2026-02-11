0115bd75e969391f1a0bdc1b42026583f96f0d35
Corey Barr <coreybarr01@gmail.com>
2026-02-11 13:40:27 +1100

    Remove neo-tree as using nvim-tree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -41,7 +41,6 @@ require('lazy').setup({
   require 'plugins.indent_line',
   require 'plugins.lint',
   require 'plugins.autopairs',
-  require 'plugins.neo-tree',
   require 'plugins.lualine',
 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
