3099ccedb4d99b0068beb8a85c8e2f84574ebb84
Corey Barr <coreybarr01@gmail.com>
2026-02-12 19:27:11 +1100

    Indent line to not show in dashboard

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -1,9 +1,20 @@
 return {
-  { -- Add indentation guides even on blank lines
+  {
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
+    event = 'BufReadPre', -- lazy-load on file read, not VimEnter
     opts = {},
+    config = function(_, opts)
+      local ibl = require 'ibl'
+      ibl.setup(opts)
+
+      -- Only attach ibl to buffers that are NOT dashboard
+      vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
+        callback = function(args)
+          local ft = vim.api.nvim_buf_get_option(args.buf, 'filetype')
+          if ft ~= 'dashboard' then ibl.attach(args.buf) end
+        end,
+      })
+    end,
   },
 }
