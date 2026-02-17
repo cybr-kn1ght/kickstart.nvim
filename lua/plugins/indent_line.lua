1f349fe841271e862a97b99eb7eb294a19ff9b02
Corey Barr <coreybarr01@gmail.com>
2026-02-17 11:15:19 +1100

    Update indent line plugin for file type exclusions

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -2,19 +2,23 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
-    event = 'BufReadPre', -- lazy-load on file read, not VimEnter
-    opts = {},
-    config = function(_, opts)
-      local ibl = require 'ibl'
-      ibl.setup(opts)
-
-      -- Only attach ibl to buffers that are NOT dashboard
-      vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
-        callback = function(args)
-          local ft = vim.api.nvim_buf_get_option(args.buf, 'filetype')
-          if ft ~= 'dashboard' then ibl.attach(args.buf) end
-        end,
-      })
-    end,
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      indent = { char = '│' }, -- choose a char you like
+      whitespace = { remove_blankline_trail = false }, -- show guides through blank lines
+      exclude = {
+        filetypes = {
+          'dashboard',
+          'help',
+          'lazy',
+          'mason',
+          'lspinfo',
+          'TelescopePrompt',
+          'TelescopeResults',
+          'terminal',
+        },
+      },
+      scope = { enabled = true },
+    },
   },
 }
